0d792e60b7d84f7e291ac5f6f63c22a83334bc78
Sean Robinson <sean@seanair.local>
2015-11-23 20:45:27 -0700

    Adding 3D touch quick actions

--- CSIS4550Final/AppDelegate.swift
+++ CSIS4550Final/AppDelegate.swift
@@ -40,6 +40,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    func application(application: UIApplication, performActionForShortcutItem shortcutItem: UIApplicationShortcutItem, completionHandler: (Bool) -> Void){
+        print("Shortcut tapped")
+    }
 
 
 }
